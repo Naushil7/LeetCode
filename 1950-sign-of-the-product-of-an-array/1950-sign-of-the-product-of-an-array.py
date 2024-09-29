2d30c81d531b3bb1078249d2073b9c871ac2bdea
Naushil Khajanchi <77967398+Naushil7@users.noreply.github.com>
2024-09-29 12:03:20 -0700

    Time: 206 ms (6.3%), Space: 11.7 MB (52.91%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def arraySign(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 1
+        for i in range(len(nums)):
+            res = res * nums[i]
+            print(res)
+        
+        if res > 0:
+            return 1
+        elif res == 0:
+            return 0
+        else:
+            return -1
+        
