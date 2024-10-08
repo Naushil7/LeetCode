2edaa5fd0bb7351ff2ebb28d632ba9a8a79aa2af
Naushil Khajanchi <77967398+Naushil7@users.noreply.github.com>
2024-10-07 19:20:30 -0700

    Time: 785 ms (8.29%), Space: 15 MB (98.75%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        # cash = {}
+        # for i in bills:
+        #     if i in cash:
+        #         cash[i] += 1
+        #     else:
+        #         cash[i] = 1
+
+        # print(cash)
+        count_5 = 0
+        count_10 = 0
+        count_20 = 0
+        for i in bills:
+            print(count_5)
+            print(count_10)
+            print(count_20)
+            print("\n")
+            if i == 5:
+                count_5 += 1
+            elif i == 10:
+                if count_5 > 0:
+                    count_5 -= 1
+                    count_10 += 1
+                else:
+                    return False
+            else:
+                if count_5 > 0 and count_10 > 0:
+                    count_5 -= 1
+                    count_10 -= 1
+                    count_20 += 1
+                elif count_5 >= 3:
+                    count_5 -= 3
+                    count_20 += 1
+                else:
+                    return False
+
+
+        return True
