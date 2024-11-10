62f73bc48040ce466239d3d29d18d393fc3d90fc
Naushil Khajanchi <77967398+Naushil7@users.noreply.github.com>
2024-11-09 16:14:04 -0800

    Time: 25 ms (19.47%), Space: 11.7 MB (97.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,21 +5,28 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        dict_1 = {}
-        dict_2 = {}
-        for i in range(len(s)):
-            if s[i] in dict_1:
-                dict_1[s[i]] += 1
-            else:
-                dict_1[s[i]] = 1
+        # dict_1 = {}
+        # dict_2 = {}
+        # for i in range(len(s)):
+        #     if s[i] in dict_1:
+        #         dict_1[s[i]] += 1
+        #     else:
+        #         dict_1[s[i]] = 1
 
-        for i in range(len(t)):
-            if t[i] in dict_2:
-                dict_2[t[i]] += 1
-            else:
-                dict_2[t[i]] = 1
+        # for i in range(len(t)):
+        #     if t[i] in dict_2:
+        #         dict_2[t[i]] += 1
+        #     else:
+        #         dict_2[t[i]] = 1
 
-        if dict_1 == dict_2:
+        # if dict_1 == dict_2:
+        #     return True
+        # else:
+        #     return False
+        s_count = Counter(s)
+        t_count = Counter(t)
+
+        if s_count == t_count:
             return True
         else:
             return False
