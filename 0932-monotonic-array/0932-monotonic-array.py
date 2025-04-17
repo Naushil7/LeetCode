7c55d3a60fc38d60e6d6acdf8ef80c4b8252ba05
Naushil Khajanchi <77967398+Naushil7@users.noreply.github.com>
2025-04-17 09:37:20 -0700

    Time: 29 ms (63.44%), Space: 28.9 MB (69.12%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.py
+++ 0932-monotonic-array/0932-monotonic-array.py
@@ -1,46 +1,17 @@
-class Solution(object):
-    def isMonotonic(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        last = 0
-        cnt = 0
-        start = 0
-        nxt = 1 
-
-        for i in range(len(nums)-1):
-            if nums[i] == nums[i+1] and nxt < len(nums)-1:
-                start += 1
-                nxt += 1
-            else:
-                break
-                
-        if len(nums) == 1:
-            return True
-
-        elif nums[start] >= nums[nxt]:
-            for i in range(len(nums)-1):
-                if nums[i] >= nums[i+1]:
-                    last = nums[i+1]
-                    cnt +=1
-                else:
-                    return False
-
-            if cnt == len(nums)-1:
-                return True
-            else:
-                return False
-
-        elif nums[start] <= nums[nxt]:
-            for i in range(len(nums)-1):
-                if nums[i] <= nums[i+1]:
-                    last = nums[i+1]
-                    cnt +=1
-                else:
-                    return False
-
-            if cnt == len(nums)-1:
-                return True
-            else:
-                return False
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        Type = 0  # 0 = not decided, 1 = decreasing, 2 = increasing
+        
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i+1]:
+                if Type == 0:
+                    Type = 1  # set as decreasing
+                elif Type == 2:
+                    return False  # conflict: was increasing before
+            elif nums[i] < nums[i+1]:
+                if Type == 0:
+                    Type = 2  # set as increasing
+                elif Type == 1:
+                    return False  # conflict: was decreasing before
+        
+        return True
